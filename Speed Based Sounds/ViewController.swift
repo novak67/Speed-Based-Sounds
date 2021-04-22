8a235bdee9b657308227ce9b5d61945d70dd4ac8
Jordan Rarrat <Jrarrat@bsd220.org>
2021-04-22 11:02:09 -0500

    made speedometer

--- Speed Based Sounds/ViewController.swift
+++ Speed Based Sounds/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    let Manager = CLLocationManager()
+    let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Manager.delegate = self
+        Manager.desiredAccuracy = kCLLocationAccuracyBest
+        Manager.requestWhenInUseAuthorization()
+        Manager.startUpdatingLocation()
+        mapView.mapType = MKMapType.hybrid
+        UIApplication.shared.isIdleTimerDisabled = true
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+       let location = locations[0]
+        
+       let center = location.coordinate
+       let region = MKCoordinateRegion(center: center, span: self.span)
+        
+       mapView.setRegion(region, animated: true)
+       self.mapView.showsUserLocation = true
+        speedLabel.text = "\(location.speed) Mph"
+    }
+    
+   
+    
+    
 
 }
 
